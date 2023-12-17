e21c9dd3e7e5eef1fe7da6f27a04ae5d23049ebf
Marcus <marcus-zh@qq.com>
2023-12-17 14:36:00 +0800

    style: support mobile and use color var()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,6 @@ import style9 from "style9";
 import React from "react";
 
 const styles = style9.create({
-  name: {
-    color: "var(--main-color)",
-    fontWeight: "bold",
-    fontSize: "60px",
-    display: "inline",
-    textShadow: "0px 8px 20px rgba(0,118,255,0.39)",
-  },
   main: {
     height: "50vh",
     width: "50vw",
@@ -19,40 +12,65 @@ const styles = style9.create({
     display: "flex",
     flexDirection: "column",
     justifyContent: "right",
+    "@media screen and (max-width: 768px)": {
+      width: "70vw",
+      height: "70vh",
+      top: "18vh",
+      right: "15vw",
+    }
   },
   titles: {
     display: "flex",
     flexDirection: "column",
     color: "var(--title-color)",
   },
+  name: {
+    color: "var(--main-color)",
+    fontWeight: "bold",
+    fontSize: "60px",
+    display: "inline",
+    textShadow: "var(--shadow)",
+  },
   span: {
     color: "var(--main-color)",
-    textShadow: "0px 8px 20px rgba(0,118,255,0.39)",
+    textShadow: "var(--shadow)",
+    transition: "all 0.1s ease 0s",
   },
   titleBig: {
     fontSize: "60px",
     fontWeight: "bold",
     marginTop: 0,
     marginBottom: "1.5rem",
+    "@media screen and (max-width: 768px)": {
+      fontSize: "50px"
+    }
   },
   titleSmall: {
     fontSize: "30px",
     fontWeight: "bold",
     marginTop: 0,
     marginBottom: "1.5rem",
+    color: "var(--text-2-color)",
   },
   buttons: {
     display: "flex",
     lineHeight: "2.8rem",
     flexDirection: "row",
+    "@media screen and (max-width: 768px)": {
+      flexDirection: "column",
+      alignItems: 'flex-start'
+    }
   },
   button: {
-    marginRight: "20px",
+    marginRight: "1rem",
+    "@media screen and (max-width: 768px)": {
+      marginBottom: "1rem",
+    },
     padding: "0 3.5rem",
     height: "3rem",
     borderRadius: "8px",
     backgroundColor: "var(--main-color)",
-    boxShadow: "0 4px 14px 0 rgba(0,118,255,0.39)",
+    boxShadow: "var(--shadow)",
     borderWidth: "medium",
     borderColor: "var(--main-color)",
     borderStyle: "solid",
@@ -61,16 +79,15 @@ const styles = style9.create({
     cursor: "pointer",
     ":hover": {
       backgroundColor: "var(--hover-color)",
-      boxShadow: "0 6px 20px rgba(0,118,255,0.23)",
+      boxShadow: "var(--hover-shadow)",
       borderColor: "var(--hover-color)",
     },
   },
   primaryButton: {
     backgroundColor: "transparent",
-    // boxShadow: "none",
     ":hover": {
       backgroundColor: "var(--hover-bg-color)",
-      boxShadow: "0 6px 20px rgba(0,118,255,0.23)",
+      boxShadow: "var(--hover-shadow)",
       borderColor: "var(--hover-bg-color)",
     },
   },
@@ -78,39 +95,84 @@ const styles = style9.create({
     color: "var(--text-color)",
     textDecoration: "none",
     fontWeight: "bold",
-    ":hover": {
-      color: "inherit",
-    },
+  },
+  footer: {
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: "1rem",
+    color: "var(--text-2-color)",
+    flexDirection: "column",
+    alignItems: "center",
+    fontSize: "0.8rem",
+    "@media screen and (max-width: 768px)": {
+      fontSize: "0.9rem"
+    }
+  },
+  footerAs: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  footerA: {
+    color: "var(--title-color)",
+    textDecoration: "none",
+    ":hover":{
+      color: "var(--main-color)",
+    }
   },
 });
 
 export default function Home() {
   return (
-    <div className={styles("main")}>
-      <div className={styles("titles")}>
-        <div className={styles("titleBig")}>
-          Hi👋,
-          <br />
-          I'm <div className={styles("name")}>Marcus</div>.
+    <>
+      <div className={styles("main")}>
+        <div className={styles("titles")}>
+          <div className={styles("titleBig")}>
+            Hi👋,
+            <br />
+            I'm <div className={`${styles("name")} name`}>Marcus</div>.
+          </div>
+          <div className={`${styles("titleSmall")} title-small`}>
+            A student
+            <span className={styles("span")}> / </span>
+            web developer
+            <span className={styles("span")}> / </span>
+            vexer
+          </div>
         </div>
-        <div className={styles("titleSmall")}>
-          A student
-          <span className={styles("span")}> / </span>
-          web developer
-          <span className={styles("span")}> / </span>
-          vexer
+        <div className={styles("buttons")}>
+          <Button text="Blog" to="https://blog.marcus233.top/" />
+          <Button text="Gtihub" to="https://github.com/MarcusYYDS" primary />
+          <Button
+            text="Bilibili"
+            to="https://space.bilibili.com/1024450661"
+            primary
+          />
         </div>
       </div>
-      <div className={styles("buttons")}>
-        <Button text="Blog" to="https://blog.marcus233.top/" />
-        <Button text="Gtihub" to="https://github.com/MarcusYYDS" primary />
-        <Button
-          text="Bilibili"
-          to="https://space.bilibili.com/1024450661"
-          primary
-        />
-      </div>
-    </div>
+      <footer className={styles("footer")}>
+        Copyright © 2022-2023 Marcus. All rights reserved.
+        <br />
+        <div className={styles("footerAs")}>
+        <a
+          href="https://github.com/MarcusYYDS/home-web"
+          target="_blank"
+          className={styles("footerA")}
+          style={{ marginRight: "1rem"}}
+        >
+          Theme: Home-web
+        </a>
+        <a
+          href="https://icp.gov.moe/?keyword=20230221"
+          target="_blank"
+          className={styles("footerA")}
+          >
+          MoeICP: 20230221
+        </a>
+        </div>
+      </footer>
+    </>
   );
 }
 type ButtonProps = {
@@ -123,12 +185,13 @@ function Button({ text, to, primary }: ButtonProps) {
     <div
       className={`${styles("button")} ${
         primary ? styles("primaryButton") : ""
-      }`}
+      } button`}
     >
       <a
         href={to}
         className={styles("buttonA")}
         style={{ color: primary ? "var(--main-color)" : "" }}
+        target="_blank"
       >
         {text}
       </a>
